bf2784a80fe33b457189f8995351f02c0539b8f6
EpicGazel <zaneshark@gmail.com>
2021-08-27 05:40:52 -0400

    Add files via upload

--- SlimeVRSensorMixer.py
+++ SlimeVRSensorMixer.py
@@ -2,6 +2,7 @@ import socket #for receiving udp data
 import struct #for making packets
 from time import perf_counter #for time delay
 from scipy.spatial.transform import Rotation #for converting Euler to quat for rotation
+from math import sqrt
 
 #delay for slowing down packet send rate. Currently unused
 def accurate_delay(delay):
@@ -20,10 +21,13 @@ gyro = [0, 0, 0] #example data
 accel = [0, 0, 0] #example data
 rotation = [0, 0, 0, 0] #example data
 
+min_max = [[180, 0], [180, 0], [180, 0]]
+
 #default values to avoid errors
 data_android = rotation 
 data_ipod = accel
 data_ipod.append(gyro) #appended as order goes accel then gyro in packets
+android_rot_quat = rotation
 
 
 #packets struct types, > is for endiness of data, most significant digit's placement
@@ -87,20 +91,73 @@ try:
                 #Decode android data (rotiation) from bytes? -> string -> floats in an array
                 data_android = [float(x) for x in (raw_data.decode(encoding='UTF-8',errors='strict').split(','))]
                 
+                #min_max calc
+                # if (data_android[0] < min_max[0][0]):
+                #     min_max[0][0] = data_android[0]
+                # if (data_android[0] > min_max[0][1]):
+                #     min_max[0][1] = data_android[0]
+                    
+                # if (data_android[1] < min_max[1][0]):
+                #     min_max[1][0] = data_android[1]
+                # if (data_android[1] > min_max[1][1]):
+                #     min_max[1][1] = data_android[1]
+                
+                # if (data_android[2] < min_max[2][0]):
+                #     min_max[2][0] = data_android[2]
+                # if (data_android[2] > min_max[2][1]):
+                #     min_max[2][1] = data_android[2]
+                
+                #Normalize android data
+                #omega_magnitude = sqrt(data_android[0] * data_android[0] + data_android[1] * data_android[1] + data_android[2] * data_android[2])
+                #data_android[0] /= omega_magnitude
+                #data_android[1] /= omega_magnitude
+                #data_android[2] /= omega_magnitude
+                
+                #temp1 = raw_data.decode(encoding='UTF-8', errors='strict')
+                #print(f"Decoded:{temp1}")
+                
+                #temp2 = temp1.split(',')
+                #print(f"Split:{temp2}")
+                 
+                #data_android = [float(x) for x in temp2]
+                #print(f"Floats:{data_android}")
+                
+                #covert to degrees for Rotation functions
+                data_android_degrees = []
+                for val in data_android:
+                    data_android_degrees.append(180 * val)
+                    
+                #print(f"Degrees:{data_android_degrees}")
+                
                 #Convert Euler data to Quaternion
-                android_rot = Rotation.from_euler('xyz', data_android, degrees=True)
-                android_rot = android_rot.as_quat()
-                #print(f"MagnetoXYZ: {data[0]:.2f}   {data[1]:.2f}   {data[2]:.2f}")
-            except ValueError: #Android app sends empty data sometimes. This catches that error.
+                android_rot_euler = Rotation.from_euler('xyz', data_android_degrees, degrees=True)
+                android_rot_quat = android_rot_euler.as_quat()
+                
+                #android_rot_vec = Rotation.from_rotvec(data_android)
+                #android_rot_quat = android_rot_vec.as_quat()
+                
+                #print(f"Rotation: {data_android[0]:.3f}   {data_android[1]:.3f}   {data_android[2]:.3f}")
+            except KeyboardInterrupt: #Android app sends empty data sometimes. This catches that error.
+                print("ValueError")
                 pass
+        
         except socket.timeout:
             print("android timeout")
         
         #Send android rotation data to SlimeVR Server (port 6969)
-        packet_data = (packet_type["rotation"], packet_id, android_rot[0], android_rot[1], android_rot[2], android_rot[3]) 
+        #try:
+        packet_data = (packet_type["rotation"], packet_id, android_rot_quat[0], android_rot_quat[1], android_rot_quat[2], android_rot_quat[3]) 
         packet = s2.pack(*packet_data)
         sock.sendall(packet)
         packet_id += 1
+        
+        #print(f"data_android:{data_android[0]:.3f}|{data_android[1]:.3f}|{data_android[2]:.3f}")
+        #print(f"euler:{android_rot_euler}")
+        #print(f"rot_vec:{android_rot_vec}")
+        print(f"quat:{android_rot_quat[0]:.3f}|{android_rot_quat[1]:.3f}|{android_rot_quat[2]:.3f}|{android_rot_quat[3]:.3f}")
+        #except NameError:
+        #    print("Rotation data skipped: NameError")
+        #    pass
         
         #accurate_delay(5)
         #print(f"Sending next packets:{packet_id}")
@@ -108,6 +165,10 @@ try:
 
 #Cleans up open ports so that kernal does not need to be restarted
 except KeyboardInterrupt:
+    pass
+
+finally:
+    #print(f"min_max: {min_max}")
     receiver.shutdown
     receiver.close()
     sock.shutdown
